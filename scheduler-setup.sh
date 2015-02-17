ff1b6037ac10a75dfc39f0d51d1e41a797eea751
saipramod <supadyay@hawk.iit.edu>
2015-02-16 23:27:06 -0600

    setup script for the scheduler

---
+++ scheduler-setup.sh
@@ -0,0 +1,29 @@
+sudo apt-get update
+sudo apt-get -y vim
+sudo apt-get -y install openjdk-7-jdk
+sudo apt-get -y install ssh
+sudo apt-get -y install rsync
+sudo apt-get -y install python-software-properties
+sudo apt-get -y install curl
+sudo apt-get -y install git
+sudo apt-get -y install unzip
+
+cd /tmp
+wget download.java.net/glassfish/4.0/release/glassfish-4.0.zip
+sudo unzip glassfish-4.0.zip -d /opt
+sudo chmod 777 -R /opt/*
+cat /opt/glassfish4/glassfish/domains/domain1/config/domain.xml | sed 's/"8080"/"80"/' > temp
+sudo cp temp /opt/glassfish4/glassfish/domains/domain1/config/domain.xml
+sudo /opt/glassfish4/bin/asadmin start-domain
+sudo rm -r sat-hadoop-api
+sudo rm -r HadoopAutomation
+git clone https://github.com/saipramod/HadoopAutomation.git
+git clone https://github.com/saipramod/sat-hadoop-api.git
+
+cd sat-hadoop-api/
+mvn clean install
+cd ..
+cd HadoopAutomation/
+mvn clean install
+sudo /opt/glassfish4/bin/asadmin deploy --force=true target/*.war
+
