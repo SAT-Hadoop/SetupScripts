0168e596f7e7c843687f2ef1ed16fea499f19ec6
saipramod <supadyay@hawk.iit.edu>
2015-02-27 17:15:05 -0600

    changes made to schduler script

--- scheduler-setup.sh
+++ scheduler-setup.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install rsync
 sudo apt-get -y install python-software-properties
 sudo apt-get -y install curl
 sudo apt-get -y install git
-sudo apt-get -y install unzip
+sudo apt-get -y install unzip maven
 
 cd /tmp
 wget download.java.net/glassfish/4.0/release/glassfish-4.0.zip
@@ -19,7 +19,9 @@ sudo rm -r sat-hadoop-api
 sudo rm -r HadoopAutomation
 git clone https://github.com/saipramod/HadoopAutomation.git
 git clone https://github.com/saipramod/sat-hadoop-api.git
-
+sudo apt-get install -y python-setuptools
+git clone https://github.com/s3tools/s3cmd.git
+python s3cmd/setup.py install
 cd sat-hadoop-api/
 mvn clean install
 cd ..
