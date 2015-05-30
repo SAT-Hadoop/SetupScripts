59ab4ce6d279f1c9df81190087cbdefe0ff5038b
jhajek <hajek@iit.edu>
2015-05-30 16:40:02 -0500

    used the filer option to reduce having to grep to find the previous instance that will be removed with the new instance

--- Packer-buildscripts/task-scheduler/eucalyptus/push-sysprepped-system-eucalyptus.sh
+++ Packer-buildscripts/task-scheduler/eucalyptus/push-sysprepped-system-eucalyptus.sh
@@ -2,13 +2,13 @@
 set -e
 set -x
 
-TYPE=production
+TYPE=task-scheduler-production
 # This is the command to deregister previous version
-# best bet is to search for client token (set this to production or testing to match the system you are replacing
+# best bet is to search for description set below (set this to production or testing to match the system you are replacing
 
-EMI=`euca-describe-instances | grep task-scheduler-$TYPE | awk {'print $3'}`
+EMI=`euca-describe-images --filter description=$TYPE | awk {'print $2'}`
 euca-deregister $EMI --debug 
  
 
 # this script will only work if you have your eucarc credentials sourced and you have VPN or on campus network access
-euca-install-image -i output-qemu/ubuntu-trusty-task-scheudler-base.raw --virtualization-type hvm -b ubuntu-trusty-task-scheudler-base -r x86_64 --name task-scheduler 
+euca-install-image -i output-qemu/ubuntu-trusty-task-scheudler-base.raw --virtualization-type hvm -b ubuntu-trusty-task-scheudler-base -r x86_64 --name task-scheduler-production --description $TYPE --progress --debug
