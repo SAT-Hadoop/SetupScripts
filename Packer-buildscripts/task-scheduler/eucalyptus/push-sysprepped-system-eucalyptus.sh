750da1f5f44a4a3b38819021548c8578711f04ff
jhajek <hajek@iit.edu>
2015-05-30 15:16:50 -0500

    Add control logic to deregister the previous task-schduler image if there is one

--- Packer-buildscripts/task-scheduler/eucalyptus/push-sysprepped-system-eucalyptus.sh
+++ Packer-buildscripts/task-scheduler/eucalyptus/push-sysprepped-system-eucalyptus.sh
@@ -2,7 +2,13 @@
 set -e
 set -x
 
-# command to deregister previous version needs to go here -- but my power is out so I can't get to the internet right now...
+TYPE=production
+# This is the command to deregister previous version
+# best bet is to search for client token (set this to production or testing to match the system you are replacing
+
+EMI=`euca-describe-instances | grep task-scheduler-$TYPE | awk {'print $3'}`
+euca-deregister $EMI --debug 
+ 
 
 # this script will only work if you have your eucarc credentials sourced and you have VPN or on campus network access
 euca-install-image -i output-qemu/ubuntu-trusty-task-scheudler-base.raw --virtualization-type hvm -b ubuntu-trusty-task-scheudler-base -r x86_64 --name task-scheduler 
