37562edff8229664457445cd91671313f15e0b07
jhajek <hajek@iit.edu>
2015-05-31 00:49:22 -0500

    Commented out the Eucalyptus KVM install commands to strip local login access

--- Packer-buildscripts/central-logging-server/vagrant/post_install_vagrant.sh
+++ Packer-buildscripts/central-logging-server/vagrant/post_install_vagrant.sh
@@ -37,9 +37,9 @@ sudo sed -i '$ a *.* ?FILENAME' /etc/rsyslog.conf
 sudo service rsyslog restart
 
 # ending configuration and poweroff
-echo password | sudo -S curl -o /etc/rc.local https://raw.githubusercontent.com/viglesiasce/cloud-images/master/utils/rc.local
-echo password | sudo -S chmod +x /etc/rc.local
+#echo password | sudo -S curl -o /etc/rc.local https://raw.githubusercontent.com/viglesiasce/cloud-images/master/utils/rc.local
+#echo password | sudo -S chmod +x /etc/rc.local
 
-echo "passwd -l vagrant" > /tmp/shutdown
+#echo "passwd -l vagrant" > /tmp/shutdown
 echo "rm /tmp/shutdown" >> /tmp/shutdown
 echo "shutdown -P now" >> /tmp/shutdown
