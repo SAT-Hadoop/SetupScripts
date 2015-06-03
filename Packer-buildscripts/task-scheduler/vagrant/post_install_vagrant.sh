6aeef77d7f9fc5e8255cd51bee68d82e22362be3
eucauser <eucauser@morse.(none)>
2015-06-03 18:53:42 -0500

    Updated Vagrant build script to reflect working eucalyptyus build script

--- Packer-buildscripts/task-scheduler/vagrant/post_install_vagrant.sh
+++ Packer-buildscripts/task-scheduler/vagrant/post_install_vagrant.sh
@@ -8,8 +8,7 @@ echo password | sudo -S add-apt-repository -y ppa:webupd8team/java
 echo password | sudo -S  apt-get update
 sudo apt-get install -y rsyslog
 
-# Oracle JDK needed for some of the glassfish packages - It has a license you ne
-ed to accpet - here is how to auto do that
+# Oracle JDK needed for some of the glassfish packages - It has a license you need to accpet - here is how to auto do that
 # http://askubuntu.com/questions/190582/installing-java-automatically-with-silent-option
 sudo echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 #http://www.himpfen.com/install-java-ubuntu/
@@ -26,9 +25,7 @@ sudo sed -i '/mcast_join = 239.2.11.71/i \ host = 192.168.98.218' /etc/ganglia/g
 sudo sed -i 's/name = "unspecified"/#name = "hadoop-cluster"/g' /etc/ganglia/gmond.conf
 sudo sed -i 's/mcast_join = 239.2.11.71/ #mcast_join = 239.2.11.71/g' /etc/ganglia/gmond.conf
 sudo sed -i 's/bind = 239.2.11.71/#bind = 239.2.11.71/g' /etc/ganglia/gmond.conf
-
 sudo sed -i 's/port = 8649/#port = 8649/g' /etc/ganglia/gmond.conf
-
 sudo sed -i 's/bind = 239.2.11.71/#bind = 239.2.11.71/g' /etc/ganglia/gmond.conf
 
 sudo service ganglia-monitor restart
@@ -40,10 +37,8 @@ sudo sed -i "$ a *.* @192.168.98.218:514" /etc/rsyslog.conf
 
 # Install Task Scheduler Application by running setup-scripts sh from Github repo
 wget --directory-prefix=/tmp https://raw.githubusercontent.com/SAT-Hadoop/SetupScripts/master/scheduler-setup.sh
-chmod u+x /tmp/scheduler-setup.sh
+chmod 777 /tmp/scheduler-setup.sh
 /tmp/scheduler-setup.sh
-
-
 
 # ending configuration and poweroff
 #echo password | sudo -S curl -o /etc/rc.local https://raw.githubusercontent.com/viglesiasce/cloud-images/master/utils/rc.local
