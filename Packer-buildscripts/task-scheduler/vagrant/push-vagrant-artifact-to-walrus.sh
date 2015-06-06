60e5835391c003eb3d2de2da01e53cb996e009e4
eucauser <eucauser@morse.(none)>
2015-06-06 01:42:24 -0500

    This script pushed the completed Vagrant box to our Walrus server

---
+++ Packer-buildscripts/task-scheduler/vagrant/push-vagrant-artifact-to-walrus.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Remove any pre-existing image
+s3cmd -v del s3://vagrant-builds/vagrant-build/packer_qemu_libvirt.box
+# This command removes the bucket
+s3cmd -v rb s3://vagrant-builds/vagrant-build
+# put the build folder for vagrant to Walrus 
+s3cmd -v --recursive --progress put vagrant-build s3://vagrant-builds
+# set the bucket to public for access for anyone
+s3cmd setacl -v -P --recursive  s3://vagrant-builds/vagrant-build/
+
