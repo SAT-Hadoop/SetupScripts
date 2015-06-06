a19eeaea28c35d6a0307e1c2b8f277e9cfeb5b62
eucauser <eucauser@morse.(none)>
2015-06-06 01:52:15 -0500

    Updated verbose output and as well as updating what is uploaded and what is deleted from Walrus - to always maintain the most recent Vagrant Box

--- Packer-buildscripts/task-scheduler/vagrant/push-vagrant-artifact-to-walrus.sh
+++ Packer-buildscripts/task-scheduler/vagrant/push-vagrant-artifact-to-walrus.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Remove any pre-existing image
-s3cmd -v del s3://vagrant-builds/vagrant-build/packer_qemu_libvirt.box
+s3cmd -v --recursive del s3://vagrant-builds/vagrant-build
 # This command removes the bucket
-s3cmd -v rb s3://vagrant-builds/vagrant-build
+#s3cmd -v del s3://vagrant-builds/vagrant-build
 # put the build folder for vagrant to Walrus 
 s3cmd -v --recursive --progress put vagrant-build s3://vagrant-builds
 # set the bucket to public for access for anyone
