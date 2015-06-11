a37cf8e309b3cf2b18bdbbdb86341277d8369a9d
Jeremy Hajek <hajek@iit.edu>
2015-06-11 11:10:11 -0500

    Updated the entire build script to do the end to end process as well as clear out any build artificts

--- Packer-buildscripts/task-scheduler/eucalyptus/end-to-end-build-process.sh
+++ Packer-buildscripts/task-scheduler/eucalyptus/end-to-end-build-process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set e
-set x
+set -e
+set -x
 
 #this script will rm -rf any existing artifacts to start fresh
 
@@ -9,15 +9,20 @@ if [ -a output-qemu ]
     rm -rf output-qemu
 fi
 
-# run the packer build script here
+# validate the packer build script here
 
-# packer validate ubuntu-packer-eucalyptus.json
+../../../../packer-tool/packer validate ubuntu-packer-eucalyptus.json
 
 # packer build ubuntu-packer-eucalyptus.json
+../../../../packer-tool/packer build ubuntu-packer-eucalyptus.json
 
 #Does the local finishing touches
-sysprep-script.sh
+./sysprep-script.sh
 
 # has code to also delete prior images and update to the new one
 
-push-sysprepped-system-eucalyptus.sh
+./push-sysprepped-system-eucalyptus.sh
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+echo "All Done"
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+echo " "
