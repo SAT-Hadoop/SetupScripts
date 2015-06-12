a2c3ac0c4941470f68b9e783d757c5b0f310f487
Jeremy Hajek <hajek@iit.edu>
2015-06-12 00:22:50 -0500

    changed filesystem from ext3 to ext4

--- disksetup.sh
+++ disksetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/sbin/mkfs -t ext3 /dev/vdb
+/sbin/mkfs -t ext4 /dev/vdb
 /bin/mkdir /vol-01
 /bin/mount /dev/vdb /vol-01
 /bin/echo "`/sbin/ifconfig | grep 'inet addr:' | head -1 | awk '{print $2}'|awk -F":" '{print $2}' `  `hostname` " >> /etc/hosts
