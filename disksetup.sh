165da5cb032c009aeb5052e886a7a84110151483
Jeremy Hajek <hajek@iit.edu>
2015-06-12 00:43:13 -0500

    added -p to mkdir to prevent the permission denied command

--- disksetup.sh
+++ disksetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /sbin/mkfs -t ext4 /dev/vdb
-/bin/mkdir /mnt/vol-01
+/bin/mkdir -p /mnt/vol-01
 /bin/mount /dev/vdb /mnt/vol-01
 /bin/echo "`/sbin/ifconfig | grep 'inet addr:' | head -1 | awk '{print $2}'|awk -F":" '{print $2}' `  `hostname` " >> /etc/hosts
