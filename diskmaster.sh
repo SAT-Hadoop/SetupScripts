51eb10ce9c3fb9b79ac8c6c21873bfa049bf4fd8
Jeremy Hajek <hajek@iit.edu>
2015-06-12 00:59:56 -0500

    updated the file path to add /mnt

--- diskmaster.sh
+++ diskmaster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-/sbin/mkfs -t ext3 /dev/vdb
-/bin/mkdir /vol-01
-/bin/mount /dev/vdb /vol-01
+/sbin/mkfs -t ext4 /dev/vdb
+/bin/mkdir -p /mnt/vol-01
+/bin/mount /dev/vdb /mnt/vol-01
 /bin/echo "`/sbin/ifconfig | grep 'inet addr:' | head -1 | awk '{print $2}'|awk -F":" '{print $2}' `  `hostname` " >> /etc/hosts
 sed '/master/d' /etc/hosts > temp
 /bin/echo "`/sbin/ifconfig | grep 'inet addr:' | head -1 | awk '{print $2}'|awk -F":" '{print $2}' `  master " >> temp
