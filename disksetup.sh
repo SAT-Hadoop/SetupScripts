d641a57bb36dab5dedc230ba7dad4529626db665
Jeremy Hajek <hajek@iit.edu>
2015-06-12 00:36:17 -0500

    added /mnt in front of vol-01

--- disksetup.sh
+++ disksetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /sbin/mkfs -t ext4 /dev/vdb
-/bin/mkdir /vol-01
-/bin/mount /dev/vdb /vol-01
+/bin/mkdir /mnt/vol-01
+/bin/mount /dev/vdb /mnt/vol-01
 /bin/echo "`/sbin/ifconfig | grep 'inet addr:' | head -1 | awk '{print $2}'|awk -F":" '{print $2}' `  `hostname` " >> /etc/hosts
