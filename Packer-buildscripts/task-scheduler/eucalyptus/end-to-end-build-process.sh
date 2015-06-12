45ec1f4307a6c91635bcfefd5b9fb8cdb88c6a03
Jeremy Hajek <hajek@iit.edu>
2015-06-12 00:01:46 -0500

    Added getopts to make sure that the type of STABLE or TEST is defined via -t

--- Packer-buildscripts/task-scheduler/eucalyptus/end-to-end-build-process.sh
+++ Packer-buildscripts/task-scheduler/eucalyptus/end-to-end-build-process.sh
@@ -4,36 +4,40 @@ set -x
 
 #this script will rm -rf any existing artifacts to start fresh
 
-if [ "$#" -eq 1 ]; then
+while getopts ":t:" opt; do
+  case $opt in
+    t)
+      #if [ "$#" -eq 1 ]; then
+      if [ -a output-qemu ]
+        then
+          rm -rf output-qemu
+      fi
 
-  if [ -a output-qemu ]
-    then
-      rm -rf output-qemu
-  fi
+       # validate the packer build script here
 
-  # validate the packer build script here
+       ../../../../packer-tool/packer validate ubuntu-packer-eucalyptus.json
 
-  ../../../../packer-tool/packer validate ubuntu-packer-eucalyptus.json
+       # packer build ubuntu-packer-eucalyptus.json
+       ../../../../packer-tool/packer build ubuntu-packer-eucalyptus.json
 
-  # packer build ubuntu-packer-eucalyptus.json
-  ../../../../packer-tool/packer build ubuntu-packer-eucalyptus.json
+       #Does the local finishing touches
+       ./sysprep-script.sh
 
-  #Does the local finishing touches
-  ./sysprep-script.sh
+       # has code to also delete prior images and update to the new one
 
-  # has code to also delete prior images and update to the new one
-
-  ./push-sysprepped-system-eucalyptus.sh $1
-  echo "++++++++++++++++++++++++++++++++++++++++++++"
-  echo "All Done"
-  echo "++++++++++++++++++++++++++++++++++++++++++++"
-  echo " "
-
-else
-
- echo "Missing system type parameter - BUILD or TEST"
-
-fi 
-
-
-
+       ./push-sysprepped-system-eucalyptus.sh $OPTARG
+       echo "++++++++++++++++++++++++++++++++++++++++++++"
+       echo "+ All Done                                 +"
+       echo "++++++++++++++++++++++++++++++++++++++++++++"
+       echo " "
+       ;;
+    \?)
+       echo "Invalid option: -$OPTARG" >&2
+       exit 1
+       ;;
+    :)
+      echo "Option -$OPTARG requires and argument STABLE or TEST." >&2
+      exit 1
+      ;;
+  esac
+done
