16748ca9bb34417fa216b6cd8a4d0ae418ed90fa
Jeremy Hajek <hajek@iit.edu>
2015-06-12 00:02:36 -0500

    Added the BASH commandline opt $1

--- Packer-buildscripts/task-scheduler/eucalyptus/push-sysprepped-system-eucalyptus.sh
+++ Packer-buildscripts/task-scheduler/eucalyptus/push-sysprepped-system-eucalyptus.sh
@@ -2,7 +2,15 @@
 set -e
 set -x
 
-TYPE=task-scheduler-production
+if [ "$#" -ne 1 ]; then
+ 
+  echo "Not enough parameters - missing the type description STABLE or TEST"
+
+else
+
+  echo "Instance type is of $1"
+
+TYPE=task-scheduler-$1
 # This is the command to deregister previous version
 # best bet is to search for description set below (set this to production or testing to match the system you are replacing
 
@@ -12,3 +20,8 @@ euca-deregister $EMI --debug
 
 # this script will only work if you have your eucarc credentials sourced and you have VPN or on campus network access
 euca-install-image -i output-qemu/ubuntu-trusty-task-scheudler-base.raw --virtualization-type hvm -b ubuntu-trusty-task-scheudler-base -r x86_64 --name task-scheduler-production --description $TYPE --progress --debug
+
+fi
+
+
+
