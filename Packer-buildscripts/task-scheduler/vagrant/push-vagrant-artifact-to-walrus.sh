bdfb0c677275d5dbd9555ec85caf32b102763f3e
Jeremy Hajek <hajek@iit.edu>
2015-06-20 23:28:53 -0500

    Changed the location of the place the vagrant box is placed so as not to clobber other vagrant boxes upon upload

--- Packer-buildscripts/task-scheduler/vagrant/push-vagrant-artifact-to-walrus.sh
+++ Packer-buildscripts/task-scheduler/vagrant/push-vagrant-artifact-to-walrus.sh
@@ -3,15 +3,15 @@ set -e
 set -x
 
 # Remove any pre-existing image
-s3cmd -v --recursive del s3://vagrant-builds/vagrant-build
+s3cmd -v --recursive del s3://vagrant-builds/vagrant-task-scheduler-build
 
 #calculate an md5hash of the vagrant box just for safety
-md5sum vagrant-build/ubuntu-task-scheduler-packer-vagrant-build-* > md5sum.txt
+md5sum vagrant-task-scheduler-build/ubuntu-task-scheduler-packer-vagrant-build-* > md5sum.txt
 
 # This command removes the bucket
 #s3cmd -v del s3://vagrant-builds/vagrant-build
 
 # put the build folder for vagrant to Walrus 
-s3cmd -v --recursive --progress put vagrant-build s3://vagrant-builds
+s3cmd -v --recursive --progress put vagrant-task-scheduler-build s3://vagrant-builds
 # set the bucket to public for access for anyone
-s3cmd setacl -v -P --recursive  s3://vagrant-builds/vagrant-build/
+s3cmd setacl -v -P --recursive  s3://vagrant-builds/vagrant-task-scheduler-build/
